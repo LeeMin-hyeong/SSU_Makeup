c5abd258226b52b88d4f745d7b5b64ea88012e4e
LeeMin-hyeong <lmhstrar070@naver.com>
2022-11-14 13:35:51 +0900

    Survey activity

---
+++ app/src/main/java/com/example/ssu_makeup/SurveyActivity.java
@@ -0,0 +1,14 @@
+package com.example.ssu_makeup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+    }
+}
