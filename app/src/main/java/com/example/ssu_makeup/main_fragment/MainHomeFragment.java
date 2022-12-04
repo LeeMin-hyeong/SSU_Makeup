bb54f52d19c65bf5a771da2625935858ee95e0fa
LeeMin-hyeong <lmhstrar070@naver.com>
2022-12-04 14:45:31 +0900

    Recommend Product frame

--- app/src/main/java/com/example/ssu_makeup/main_fragment/MainHomeFragment.java
+++ app/src/main/java/com/example/ssu_makeup/main_fragment/MainHomeFragment.java
@@ -3,20 +3,30 @@ package com.example.ssu_makeup.main_fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ssu_makeup.Product;
+import com.example.ssu_makeup.ProductAdaptor;
 import com.example.ssu_makeup.R;
 
-public class MainHomeFragment extends Fragment {
+import java.util.ArrayList;
 
+public class MainHomeFragment extends Fragment {
+    ArrayList<Product> productRecommendArrayList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_main_home, container, false);
 
+        RecyclerView recyclerView = root.findViewById(R.id.main_product_recommend_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new ProductAdaptor(productRecommendArrayList, requireContext()));
 
+        return root;
     }
 }
