c0a00f661981623450013f63157e8e905917df7d
LeeMin-hyeong <lmhstrar070@naver.com>
2022-12-04 23:56:04 +0900

    Optimization

--- app/src/main/java/com/example/ssu_makeup/main_fragment/MainHomeFragment.java
+++ app/src/main/java/com/example/ssu_makeup/main_fragment/MainHomeFragment.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.ssu_makeup.Product;
 import com.example.ssu_makeup.ProductAdaptor;
@@ -18,10 +19,13 @@ import java.util.ArrayList;
 
 public class MainHomeFragment extends Fragment {
     ArrayList<Product> productRecommendArrayList;
+    TextView recommendationMessage;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_main_home, container, false);
+        //TODO: skinType, lastName, firstName 불러오기
+        recommendationMessage = root.findViewById(R.id.recommendation_message);
+        recommendationMessage.setText(getString(R.string.recommendation_message, "skinType", "lastName", "firstName"));
 
 //        RecyclerView recyclerView = root.findViewById(R.id.main_product_recommend_recycler_view);
 //        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
--- app/src/main/java/com/example/ssu_makeup/survey_fragment/SurveyResultFragment.java
+++ app/src/main/java/com/example/ssu_makeup/survey_fragment/SurveyResultFragment.java
@@ -1,7 +1,6 @@
 package com.example.ssu_makeup.survey_fragment;
 
 import android.content.Intent;
-import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +11,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.ssu_makeup.Baumann;
