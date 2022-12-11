4c7aaa0330f081f46e7529ca31748c73308b105f
TaeYeon <newthink0203@naver.com>
2022-12-12 02:17:30 +0900

    show to recommend product lists

--- app/src/main/java/com/example/ssu_makeup/survey_fragment/survey_q1/SurveyQ1_1Fragment.java
+++ app/src/main/java/com/example/ssu_makeup/survey_fragment/survey_q1/SurveyQ1_1Fragment.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 import com.example.ssu_makeup.R;
-import com.example.ssu_makeup.Baumann;
+
 import com.example.ssu_makeup.SurveyActivity;
 
 public class SurveyQ1_1Fragment extends Fragment implements View.OnClickListener{
@@ -19,6 +19,8 @@ public class SurveyQ1_1Fragment extends Fragment implements View.OnClickListener
     Button answer2;
     Button answer3;
     Button answer4;
+//    Button answer5;
+    int answer=0;
 
     public static SurveyQ1_1Fragment newInstance() {
         return new SurveyQ1_1Fragment();
@@ -31,18 +33,45 @@ public class SurveyQ1_1Fragment extends Fragment implements View.OnClickListener
         answer2 = root.findViewById(R.id.answer2);
         answer3 = root.findViewById(R.id.answer3);
         answer4 = root.findViewById(R.id.answer4);
+//        answer5 = root.findViewById(R.id.answer5);
 
         question.setText(R.string.q1_1);
         answer1.setText(R.string.a1_1_1);
         answer2.setText(R.string.a1_1_2);
         answer3.setText(R.string.a1_1_3);
         answer4.setText(R.string.a1_1_4);
+//        answer5.setText(R.string.a1_1_5);
 
         answer1.setOnClickListener(this);
         answer2.setOnClickListener(this);
         answer3.setOnClickListener(this);
         answer4.setOnClickListener(this);
+//        answer5.setOnClickListener(this);
 
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer=1;
+            }
+        });
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer=2;
+            }
+        });
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer=3;
+            }
+        });
+        answer4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer=4;
+            }
+        });
         return root;
     }
 
@@ -50,7 +79,6 @@ public class SurveyQ1_1Fragment extends Fragment implements View.OnClickListener
     public void onClick(View view) {
         setButtons();
         view.setBackgroundResource(R.drawable.round_corners_selected);
-        Baumann.addScore4Answers(view,1,1);
         ((SurveyActivity)requireActivity()).replaceFragment(SurveyQ1_2Fragment.newInstance());
     }
 
@@ -59,5 +87,6 @@ public class SurveyQ1_1Fragment extends Fragment implements View.OnClickListener
         answer2.setBackgroundResource(R.drawable.round_corners);
         answer3.setBackgroundResource(R.drawable.round_corners);
         answer4.setBackgroundResource(R.drawable.round_corners);
+//        answer5.setBackgroundResource(R.drawable.round_corners);
     }
 }
