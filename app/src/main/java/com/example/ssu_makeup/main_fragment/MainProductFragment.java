314408858d4ce7c98c8fbb6fb9ddb2f6428105da
00yhsp <00yhsp@naver.com>
2022-12-12 23:57:22 +0900

    Register Review

--- app/src/main/java/com/example/ssu_makeup/main_fragment/MainProductFragment.java
+++ app/src/main/java/com/example/ssu_makeup/main_fragment/MainProductFragment.java
@@ -2,24 +2,36 @@ package com.example.ssu_makeup.main_fragment;
 
 import static com.bumptech.glide.Glide.*;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ssu_makeup.activity.LoginActivity;
+import com.example.ssu_makeup.activity.SurveyActivity;
 import com.example.ssu_makeup.adaptor.ProductReviewAdaptor;
 import com.example.ssu_makeup.custom_class.Product;
 import com.example.ssu_makeup.R;
 import com.example.ssu_makeup.custom_class.Review;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -30,24 +42,39 @@ public class MainProductFragment extends Fragment implements View.OnClickListene
     ImageView starButton4;
     ImageView starButton5;
     Button submitReviewButton;
+    EditText review;
     int reviewScore=0;
+
+    Product selectedProduct;
+    String productCategory, productIndex;
+
+    private FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference cosmeticDataRef = mFirebaseDatabase.getReference("cosmeticData");
+    private DatabaseReference userDataRef = mFirebaseDatabase.getReference("Users");
+    private FirebaseAuth mfirebase = FirebaseAuth.getInstance();
+    private String uid = mfirebase.getCurrentUser().getUid();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main_product, container, false);
 
         assert getArguments() != null;
-        Product selectedProduct = (Product)getArguments().getSerializable("selected_product");
+        selectedProduct = (Product)getArguments().getSerializable("selected_product");
         with(requireContext()).load(selectedProduct.getProductImageURL()).into((ImageView)root.findViewById(R.id.product_item_image));
         root.findViewById(R.id.product_item_image).setClipToOutline(true);
         ((TextView)root.findViewById(R.id.product_item_brand)).setText(selectedProduct.getProductBrand());
         ((TextView)root.findViewById(R.id.product_item_name)).setText(selectedProduct.getProductName());
         ((TextView)root.findViewById(R.id.product_item_ingredient)).setText(selectedProduct.getProductIngredient());
 
+        productCategory = selectedProduct.getProductCategory();
+        productIndex = selectedProduct.getProductIndex();
+
         starButton1 = root.findViewById(R.id.review_star_button_1);
         starButton2 = root.findViewById(R.id.review_star_button_2);
         starButton3 = root.findViewById(R.id.review_star_button_3);
         starButton4 = root.findViewById(R.id.review_star_button_4);
         starButton5 = root.findViewById(R.id.review_star_button_5);
+        review = root.findViewById(R.id.review_edit_text);
         submitReviewButton = root.findViewById(R.id.review_submit_button);
         starButton1.setOnClickListener(this);
         starButton2.setOnClickListener(this);
@@ -102,13 +129,30 @@ public class MainProductFragment extends Fragment implements View.OnClickListene
             starButton5.setImageResource(R.drawable.ic_review_star_red);
             reviewScore=5;
         }
-        else if(view == submitReviewButton){
-            if(reviewScore==0)
-                Toast.makeText(requireContext(), "별점을 선택해 주세요", Toast.LENGTH_SHORT).show();
-            else{
-                //TODO: 리뷰 등록 구현
+
+        submitReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(reviewScore==0)
+                    Toast.makeText(requireContext(), "별점을 선택해 주세요", Toast.LENGTH_SHORT).show();
+                else{
+                    Log.d("ProductCategory", productCategory);
+                    Log.d("ProductIndex", productIndex);
+
+                    userDataRef.child(uid).child("firstName").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String firstName = snapshot.getValue(String.class);
+                            Review currentUserReview = new Review(reviewScore, firstName, review.getText().toString());
+                            cosmeticDataRef.child(productCategory).child(productIndex).child("reviewList").push().setValue(currentUserReview);
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {}
+                    });
+
+                }
             }
-        }
+        });
     }
 
     void setStarButton(){
