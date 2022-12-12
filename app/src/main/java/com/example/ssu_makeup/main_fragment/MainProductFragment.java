b913773d3711a0ad8d2dca60e00c0a305fdad0b9
DESKTOP-DCBHHIB\1 <newthink0203@naver.com>
2022-12-13 00:05:44 +0900

    load data for reviewpage

--- app/src/main/java/com/example/ssu_makeup/main_fragment/MainProductFragment.java
+++ app/src/main/java/com/example/ssu_makeup/main_fragment/MainProductFragment.java
@@ -4,6 +4,7 @@ import static com.bumptech.glide.Glide.*;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,8 +21,17 @@ import com.example.ssu_makeup.adaptor.ProductReviewAdaptor;
 import com.example.ssu_makeup.custom_class.Product;
 import com.example.ssu_makeup.R;
 import com.example.ssu_makeup.custom_class.Review;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 public class MainProductFragment extends Fragment implements View.OnClickListener {
     ImageView starButton1;
@@ -30,18 +40,25 @@ public class MainProductFragment extends Fragment implements View.OnClickListene
     ImageView starButton4;
     ImageView starButton5;
     Button submitReviewButton;
-    int reviewScore=0;
+    int reviewScore = 0;
+    Product selectedProduct;
+    String productCategory;
+    String productIndex;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main_product, container, false);
 
         assert getArguments() != null;
-        Product selectedProduct = (Product)getArguments().getSerializable("selected_product");
-        with(requireContext()).load(selectedProduct.getProductImageURL()).into((ImageView)root.findViewById(R.id.product_item_image));
+        Product selectedProduct = (Product) getArguments().getSerializable("selected_product");
+        productCategory = selectedProduct.getProductCategory();
+        productIndex = selectedProduct.getProductIndex();
+        with(requireContext()).load(selectedProduct.getProductImageURL()).into((ImageView) root.findViewById(R.id.product_item_image));
         root.findViewById(R.id.product_item_image).setClipToOutline(true);
-        ((TextView)root.findViewById(R.id.product_item_brand)).setText(selectedProduct.getProductBrand());
-        ((TextView)root.findViewById(R.id.product_item_name)).setText(selectedProduct.getProductName());
-        ((TextView)root.findViewById(R.id.product_item_ingredient)).setText(selectedProduct.getProductIngredient());
+        ((TextView) root.findViewById(R.id.product_item_brand)).setText(selectedProduct.getProductBrand());
+        ((TextView) root.findViewById(R.id.product_item_name)).setText(selectedProduct.getProductName());
+        ((TextView) root.findViewById(R.id.product_item_ingredient)).setText(selectedProduct.getProductIngredient());
 
         starButton1 = root.findViewById(R.id.review_star_button_1);
         starButton2 = root.findViewById(R.id.review_star_button_2);
@@ -56,14 +73,29 @@ public class MainProductFragment extends Fragment implements View.OnClickListene
         starButton5.setOnClickListener(this);
         submitReviewButton.setOnClickListener(this);
 
-        ArrayList<Review> reviewArrayList = new ArrayList<>();
-        //TODO: 리뷰 불러오기
-        reviewArrayList.add(new Review(4,"김숭실","추천해요!"));
 
-        RecyclerView recyclerView = root.findViewById(R.id.product_review_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        ProductReviewAdaptor productReviewAdaptor = new ProductReviewAdaptor(reviewArrayList);
-        recyclerView.setAdapter(productReviewAdaptor);
+        //TODO: 리뷰 불러오기
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("cosmeticData").child(productCategory).child(productIndex).child("reviewList");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<Review> reviewArrayList = new ArrayList<>();
+                reviewArrayList= new ArrayList<>();
+                reviewArrayList.add(new Review(4, "김숭실", "추천해요!"));
+                for(DataSnapshot dataSnapshot:snapshot.getChildren()) {
+                    Review review = new Review(dataSnapshot.child("reviewScore").getValue(Integer.class), dataSnapshot.child("reviewer").getValue(String.class), dataSnapshot.child("review").getValue(String.class));
+                    reviewArrayList.add(review);
+                }
+                RecyclerView recyclerView = root.findViewById(R.id.product_review_recycler_view);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                ProductReviewAdaptor productReviewAdaptor = new ProductReviewAdaptor(reviewArrayList);
+                recyclerView.setAdapter(productReviewAdaptor);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
 
         return root;
     }
@@ -72,51 +104,45 @@ public class MainProductFragment extends Fragment implements View.OnClickListene
     public void onClick(View view) {
         setStarButton();
 
-        if(view == starButton1){
+        if (view == starButton1) {
             starButton1.setImageResource(R.drawable.ic_review_star_red);
-            reviewScore=1;
-        }
-        else if(view == starButton2){
+            reviewScore = 1;
+        } else if (view == starButton2) {
             starButton1.setImageResource(R.drawable.ic_review_star_red);
             starButton2.setImageResource(R.drawable.ic_review_star_red);
-            reviewScore=2;
-        }
-        else if(view == starButton3){
+            reviewScore = 2;
+        } else if (view == starButton3) {
             starButton1.setImageResource(R.drawable.ic_review_star_red);
             starButton2.setImageResource(R.drawable.ic_review_star_red);
             starButton3.setImageResource(R.drawable.ic_review_star_red);
-            reviewScore=3;
-        }
-        else if(view == starButton4) {
+            reviewScore = 3;
+        } else if (view == starButton4) {
             starButton1.setImageResource(R.drawable.ic_review_star_red);
             starButton2.setImageResource(R.drawable.ic_review_star_red);
             starButton3.setImageResource(R.drawable.ic_review_star_red);
             starButton4.setImageResource(R.drawable.ic_review_star_red);
-            reviewScore=4;
-        }
-        else if(view == starButton5) {
+            reviewScore = 4;
+        } else if (view == starButton5) {
             starButton1.setImageResource(R.drawable.ic_review_star_red);
             starButton2.setImageResource(R.drawable.ic_review_star_red);
             starButton3.setImageResource(R.drawable.ic_review_star_red);
             starButton4.setImageResource(R.drawable.ic_review_star_red);
             starButton5.setImageResource(R.drawable.ic_review_star_red);
-            reviewScore=5;
-        }
-        else if(view == submitReviewButton){
-            if(reviewScore==0)
+            reviewScore = 5;
+        } else if (view == submitReviewButton) {
+            if (reviewScore == 0)
                 Toast.makeText(requireContext(), "별점을 선택해 주세요", Toast.LENGTH_SHORT).show();
             else{
                 //TODO: 리뷰 등록 구현
             }
         }
     }
-
-    void setStarButton(){
-        starButton1.setImageResource(R.drawable.ic_review_star_grey);
-        starButton2.setImageResource(R.drawable.ic_review_star_grey);
-        starButton3.setImageResource(R.drawable.ic_review_star_grey);
-        starButton4.setImageResource(R.drawable.ic_review_star_grey);
-        starButton5.setImageResource(R.drawable.ic_review_star_grey);
-    }
+        void setStarButton () {
+            starButton1.setImageResource(R.drawable.ic_review_star_grey);
+            starButton2.setImageResource(R.drawable.ic_review_star_grey);
+            starButton3.setImageResource(R.drawable.ic_review_star_grey);
+            starButton4.setImageResource(R.drawable.ic_review_star_grey);
+            starButton5.setImageResource(R.drawable.ic_review_star_grey);
+        }
 
 }
